e184718cc4c5af2d53e011f6fc95e2444239f281
delloff <aal.daniloff@gmail.com>
2019-03-22 21:43:27 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,118 +1,25 @@
-import random
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+from classes.game import Person, bcolors
 
 
-class Person:
-    def __init__(self, hp, mp, atk, df, magic):
-        self.maxhp = hp
-        self.hp = hp
-        self.maxmp = mp
-        self.mp = mp
-        self.atkl = atk - 10
-        self.atkh = atk + 10
-        self.df = df
-        self.magic = magic
-        self.actions = ["Attack", "Magic"]
-
-    def generate_damage(self):
-        return random.randrange(self.atkl, self.atkh)
-
-    def generate_spell_damage(self, i):
-        mgl = self.magic[i]["dmg"] - 5
-        mgh = self.magic[i]["dmg"] + 5
-        return random.randrange(mgl, mgh)
-
-    def take_damage(self, dmg):
-        self.hp -= dmg  # takes damage amount from the hp
-        if self.hp < 0:
-            self.hp = 0
-        return self.hp
-
-    def get_hp(self):
-        return self.hp
-
-    def max_hp(self):
-        return self.max_hp
-
-    def get_mp(self):
-        return self.mp
-
-    def get_max_mp(self):
-        return self.maxmp
-
-    def reduce_mp(self, cost):
-        self.mp -= cost
-
-    def get_spell_name(self, i):        #returns data to the user depending on what they have chosen
-        return self.magic[i]["name"]
-
-    def get_spell_mp_cost(self, i):
-        return self.magic[i]["cost"]
-
-    '''
-    here we are choosing
-    action or magic:
-    '''
-
-    def choose_action(self):
-        i = 1
-        print("Actions")
-        for item in self.actions:
-            print(str(i) + ":", item)
-            i += 1
-
-        '''
-            and here if we choose magic
-            we also choose kind of magic
-            '''
-
-    def choose_magic(self):
-        i = 1
-        print("Magic")
-        for spell in self.magic:
-            print(str(i) + ":", spell["name"], "(cost:", str(spell["mp"]) + ")")
-            i += 1
+magic = [{"name": "Fire", "cost": 10, "dmg": 60},
+         {"name": "Thunder", "cost": 10, "dmg": 80},
+         {"name": "Blizzard", "cost": 10, "dmg": 60}]
 
 
+player = Person(460, 65, 60, 34, magic)
+enemy = Person(1200, 65, 45, 25, magic)
 
+running = True
+i = 0
 
+print(bcolors.FAIL + bcolors.BOLD + "AN ENEMY ATTACKS!" + bcolors.ENDC)
 
+while running:
+    print("=======++++++========")
+    player.choose_action()
+    choice = input("Choose action:")
+    index = int(choice) - 1
 
+    print("You chose", player.get_spell_name(int(index)))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    running = False
